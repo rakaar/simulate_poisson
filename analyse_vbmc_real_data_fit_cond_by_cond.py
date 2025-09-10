feat: fix probability normalization and time range adjustments in Skellam model

analyse_vbmc_real_data_fit_cond_by_cond.py
@@ -393,8 +393,13 @@ def plot_rt_dists_grid(
                     #     t_pts, V_A, theta_A, t_A_aff, t_stim_vals, t_E_aff, del_go, R_mean, L_mean, int(theta_best), c_A_trunc_time
                     # )
                     # V2 version
-                    t_pts_0_1 = np.arange(-5, 5+0.001, 0.001)
+                    t_pts_0_1 = np.arange(0, 1+0.001, 0.001)
                     # up_theory_mean_norm, down_theory_mean_norm = up_or_down_hit_wrt_tstim_V2(t_pts_0_1, V_A, theta_A, t_A_aff, t_stim_vals, t_E_aff, del_go, R_mean, L_mean, int(theta_best), c_A_trunc_time)
+                    ### TEMP NOTE ##############
+                    # t_A_aff = 0
+                    # t_E_aff = 0
+                    # del_go = 0
+                    # t_stim_vals = np.zeros_like(t_stim_vals)
                     up_theory_mean_norm, down_theory_mean_norm = up_or_down_hit_wrt_tstim_V3(t_pts_0_1, V_A, theta_A, t_A_aff, t_stim_vals, t_E_aff, del_go, R_mean, L_mean, int(theta_best), c_A_trunc_time)
                     ax.step(t_pts_0_1, up_theory_mean_norm, where='post', color='C1', label='THEORY +1')
                     ax.step(t_pts_0_1, -down_theory_mean_norm, where='post', color='C1', linestyle='--', label='THEORY -1')

vbmc_skellam_utils.py
@@ -149,6 +149,7 @@ def fpt_cdf_skellam(t, mu1, mu2, theta):
     # For negative times, CDF remains 0 (as initialized)
     cdf_arr = 1.0 - s0
     cdf_arr = np.clip(cdf_arr, 0.0, 1.0)
+    cdf_arr[~non_negative_mask] = 0.0 
 
     # Return scalar if input was scalar
     if np.isscalar(t):
@@ -507,7 +508,9 @@ def truncated_cum_A_t_fn(t, V_A, theta_A, trunc_time):
         survival_prob = 1.0 - cdf_at_trunc
         # Avoid division by zero
         if survival_prob > 0:
-            result[mask] = (cum_A_t_fn(t[mask], V_A, theta_A) - cdf_at_trunc) / survival_prob
+            # result[mask] = (cum_A_t_fn(t[mask], V_A, theta_A) - cdf_at_trunc) / survival_prob
+            result[mask] = (cum_A_t_fn(t[mask], V_A, theta_A)) / survival_prob
+
     
     # Return scalar if input was scalar
     if np.isscalar(t):
@@ -532,7 +535,7 @@ def up_or_down_hit_wrt_tstim_V3(t_pts_wrt_stim, V_A, theta_A, t_A_aff, t_stim_sa
         area_down = np.trapezoid(down1, t_pts_wrt_stim)
 
         total_area = area_up + area_down
-        print(f'total area = {total_area}')
+        # print(f'total area = {total_area}')
 
         up_samples[idx, :] = up1 / total_area
         down_samples[idx, :] = down1 / total_area